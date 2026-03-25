3d698ea6a91560efaaa2afb7fcb374499ab06258
Lyes Madani <lyes.madani@gmail.com>
2026-03-25 12:01:39 +0100

    Ajout du portfolio Streamlit

---
+++ main.py
@@ -0,0 +1,39 @@
+print("setup ok")
+print("setup    " " ok")
+print("test" "")
+x = 1
+y = 2
+print(test)
+import os
+
+a = 1
+a = 2
+
+
+def test():
+    print("ok")
+    if True:
+        print("oops")
+
+
+def analyse_valeur(x):
+    print("Début analyse")
+
+    if x > 10:
+        print("Valeur supérieure à 10")
+        if x > 100:
+            print("Valeur très élevée")
+    else:
+        print("Valeur inférieure ou égale à 10")
+
+    print("Fin analyse")
+
+
+analyse_valeur(5)
+analyse_valeur(50)
+analyse_valeur(150)
+import os
+import sys
+
+x = 10
+print(x)
